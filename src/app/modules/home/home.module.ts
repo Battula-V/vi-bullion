73528d39e409117f1b78aa66e0e832160bdbac4a
venkatesh <battulavenkatesh2169@gmail.com>
2024-08-05 12:16:01 +0530

    invoice component created

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -16,7 +16,9 @@ import {MatTabsModule} from '@angular/material/tabs';
 import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule,ReactiveFormsModule } from "@angular/forms";
-import {MatSelectModule} from '@angular/material/select'; 
+import {MatSelectModule} from '@angular/material/select';
+import { InvoiceComponent } from './components/invoice/invoice.component'; 
+
 
 @NgModule({
   declarations: [
@@ -24,7 +26,8 @@ import {MatSelectModule} from '@angular/material/select';
     CategoryComponent,
     SubCategoryComponent,
     ProductsComponent,
-    DashboardComponent
+    DashboardComponent,
+    InvoiceComponent
   ],
   imports: [
     CommonModule,
